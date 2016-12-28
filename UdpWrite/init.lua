2db6098696fca2fe181c9201f99562dab706b294
KeyMove <keymove@live.com>
2016-12-28 10:36:28 +0800

    lua文件更新

--- UdpWrite/init.lua
+++ UdpWrite/init.lua
@@ -1,31 +1,17 @@
-function init()
 uart.setup(0,115200,8,0,1,0)
 wifi.setmode(3)
-wifi.ap.config({ssid="Car-"..node.chipid(),pwd="12345678"})
-if file.open("cfg") then
-local ssid=file.readline()
-local pwd=file.readline()
-if not ssid or not pwd then return end
-wifi.sta.config(ssid,pwd)
-return 1
-end
-end
-
-function saveConfig()
-local n,p=wifi.sta.getconfig()
-file.remove("cfg")
-file.open("cfg","w+")
-file.writeline(n)
-file.writeline(p)
-file.flush()
-file.close()
-end
-
+wifi.ap.config({ssid="Dev-"..node.chipid(),pwd="12345678"})
+TimeOut=0
+MaxTimeOut=30
 PreRecv=function() return false end
+UDPServer=nil
+function init()
+if UDPServer==nil then
 UDPServer=net.createServer(net.UDP,5)
 UDPServer:listen(2333,nil)
 UDPServer:on("receive",function(c,d)
 user=c
+TimeOut=0
 if PreRecv(d) then return end  
 local str={}
 local op=print
@@ -35,8 +21,12 @@ if not f then table.insert(str,"\r\n") table.insert(str,v) end
 c:send(table.concat(str))
 print=op
 end)
+end
+wifi.sta.connect()
+tmr.alarm(6, 1000, tmr.ALARM_AUTO, function() TimeOut=TimeOut+1 if TimeOut>MaxTimeOut then wifi.sta.connect() TimeOut=0 end end)
+end
 
 init()
-if file.open("app.lua") then
+if file.open("app.lua") and adc.read(0)==1024 then
 dofile("app.lua")
 end
